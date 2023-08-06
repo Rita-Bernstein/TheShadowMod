dabd3d3be1642d548b369827878c52dfb4d545dc
10000volts <602908228@QQ.com>
2023-08-06 20:07:10 +0800

    碎镜，每次战斗重置
    碎镜，每次战斗重置

--- src/main/java/TheShadowMod/relics/TheShadow/BrokenMirror.java
+++ src/main/java/TheShadowMod/relics/TheShadow/BrokenMirror.java
@@ -1,13 +1,18 @@
 package TheShadowMod.relics.TheShadow;
 
 import TheShadowMod.TheShadowMod;
+import TheShadowMod.powers.TheShadow.HeavyPower;
 import TheShadowMod.relics.AbstractShadowModRelic;
 import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.mod.stslib.relics.OnPlayerDeathRelic;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
@@ -56,6 +61,11 @@ public class BrokenMirror extends AbstractShadowModRelic implements NotDeadRelic
                 .ifPresent(index -> instantObtain(player, index, true));
 
         (AbstractDungeon.getCurrRoom()).rewardPopOutTimer = 0.25F;
+    }
+
+    @Override
+    public void atBattleStart() {
+        this.counter = 2;
     }
 
     @Override
